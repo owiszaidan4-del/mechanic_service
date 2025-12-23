1c7c8675d2bc4314eab4c1f8737f3134d053dbcc
owis zaidan <owiszaidan4@gmail.com>
2025-12-24 00:23:52 +0300

    set List of type notification

--- lib/views/notifacation.dart
+++ lib/views/notifacation.dart
@@ -4,6 +4,47 @@ import 'package:flutter/widgets.dart';
 class Notifacation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text("الرسائل"), centerTitle: true),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(
+            height: 30,
+            child: DefaultTextStyle(
+              style: TextStyle(
+                fontSize: 10,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+              child: Directionality(
+                textDirection: TextDirection.rtl,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: notification_ListOf_TypeMessage(),
+                ),
+              ),
+            ),
+          ),
+          Divider(color: Colors.grey, height: 10, thickness: 3),
+        ],
+      ),
+    );
+  }
+}
+
+class notification_ListOf_TypeMessage extends StatelessWidget {
+  const notification_ListOf_TypeMessage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        GestureDetector(onTap: () {}, child: Text("جميع الرسائل(0)")),
+        GestureDetector(onTap: () {}, child: Text("تنبيهات(0)")),
+        GestureDetector(onTap: () {}, child: Text("العروض(0)")),
+      ],
+    );
   }
 }
