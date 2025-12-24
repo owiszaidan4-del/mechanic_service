fb874e2a1f9abf69d8eb319b794df9f3efb19362
owis zaidan <owiszaidan4@gmail.com>
2025-12-24 21:43:19 +0300

    creat new view Performence

---
+++ lib/views/performance.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Performance extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [Text("الاداء الشهري")]);
+  }
+}
