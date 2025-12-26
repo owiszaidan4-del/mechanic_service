087d1bf0ce7c302262a115e31ed421701603deea
owis zaidan <owiszaidan4@gmail.com>
2025-12-26 14:20:30 +0300

    configurtion firaBase

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,12 @@ import 'package:car_serves/views/signIn.dart';
 import 'package:car_serves/views/regesterView.dart';
 import 'package:car_serves/views/regester_2.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const car_serves());
 }
 
